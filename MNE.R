d4a903fe67f6ff726624676009fae41e0e1f31d8
Juan M. Barrios <juan.barrios@conabio.gob.mx>
2017-05-26 12:34:30 -0500

    Funcion de limpieza

--- MNE.R
+++ MNE.R
@@ -3,6 +3,33 @@ library("raster")
 library("fuzzySim")
 library("ENMeval")
 library("ROCR")
+
+#' Funcion para crear un archivo sin registros duplicados 
+#'
+#' @param csvFilePath Ruta del archivo de registros en formato csv
+#' @param outCsvFilePath Ruta del archivo donde se guardaran los resultados
+#' @param longColumnName Nombre de la columna donde se encuentra la longitud
+#' @param latColumnName Nombre de la columna donde se encuentra la latitud
+#' @param threshold Máxima distancia entre puntos para que se consideren
+#'                  duplicados
+#' @return Ruta del archivo con los registros limpios
+CreateCleanData <- function(csvFilePath, outCsvFilePath = "",
+                            longColumnName, latColumnName,
+                            threshold = 0.00833333333) {
+  data <- read.csv(csvFilePath, header = TRUE)
+  data_clean <- clean_dup(data, longColumnName, latColumnName, threshold)
+
+  if (outCsvFilePath == "") {
+    outCsvFilePath <- paste0(tools::file_path_sans_ext(csvFilePath),
+                             "_clean.csv")
+  }
+
+  write.csv(data_clean, file = outCsvFilePath, row.names = FALSE)
+
+  return(outCsvFilePath)
+}
+
+
 setwd("J:/USUARIOS/DarwinUICN/reuniones_talleres/taller lista roja/Resultados_TLR/Mapas_validos/MapasValidosPorGenero")
 source("clean_dup.R")
 
