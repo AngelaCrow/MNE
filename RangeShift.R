f38741aa603f3a257ac1a718e4dd9445f8ae5674
Juan M. Barrios <juan.barrios@conabio.gob.mx>
2017-10-17 10:03:58 -0500

    Funcion reportaFuturo

--- RangeShift.R
+++ RangeShift.R
@@ -1,3 +1,37 @@
+#' Report statistics about region gain and loss for future environment
+#' 
+#' @param raster_presente_bin Raster object with values 0, 1 that shows the
+#'                            current species distribution
+#' @param raster_futuro_bin Raster object with values 0, 1 that show the future
+#'                          ditribution
+#'
+#' @return A list with the frequency stats for habitat gain or loss coded as 
+#'         follows:
+#'         \itemize{
+#'           \item{0 no habitat in neither two times}
+#'           \item{1 habitat loss}
+#'           \item{10 habitat gain}
+#'           \item{11 preserved habitat} 
+#'         }
+#'         and a raster with the same coding.
+#' 
+#' @examples
+#' \dontrun{
+#' r_pres <- raster('present.tif')
+#' r_fut <- raster('future.tif')
+#' habitat_change <- reportaFuturo(r_pres, r_fut)
+#' plot(habitat_change$raster_cambio)
+#' print(habitat_change$stats)
+#' }
+reportaFuturo <- function(raster_presente_bin, raster_futuro_bin){
+  reclass_matrix <- matrix(c(0, 1, 0, 10), ncol=2)
+  offset_raster <- raster::reclassify(raster_futuro_bin, reclass_matrix)
+  
+  result <- raster_presente_bin + offset_raster
+  
+  return(list(stats=raster::freq(result, useNA='no'),
+              raster_cambio=result))
+}
 sp_models <- list.files(path = "acutiacuti",
                         pattern = "*min_L_0.5.tif$",full.names = TRUE)
 sp_models
